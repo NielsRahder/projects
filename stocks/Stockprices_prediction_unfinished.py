c64ba56e0335e29dd328b68f353987c94aca0142
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2023-10-30 15:52:23 +0100

    Organized files

--- stocks/Stockprices_prediction_unfinished.py
+++ stocks/Stockprices_prediction_unfinished.py
@@ -71,3 +71,6 @@ x_test = []
 
 for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x - prediction_days: x , 0 ])
+
+
+x_test = np.array(x_test)
---
+++ temp.py
@@ -0,0 +1,20 @@
+req = 4 * 60 * 60 * 24
+freq = 1 
+r = 1
+S = 1 
+
+N = (req * S) / (r * freq)
+print(N)
+
+
+# per dag 
+req = (1/2) * 60 * 60 * 24
+freq = 2
+N = (req * S) / (r * freq)
+
+print(req)
+print(N)
+
+print(((1/2) * 1) / (1 * (1/7)))
+print(24*7)
+
