6d2454a3260e4bebf313184223eff0526fa8cca4
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2023-11-06 10:32:15 +0100

    new lecture

--- neural_networks/Neural_network_unfinished.py
+++ neural_networks/Neural_network_unfinished.py
@@ -2,33 +2,24 @@ import numpy as np
 import sys 
 import matplotlib 
 
-inputs = [[1, 2, 3, 2.5], 
-          [2.0, 5.0, -1.0, 2.0], 
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-#for each input there is a unique weight, but the bias is for each neuron specifically so it is only one
+X = [[1, 2, 3, 2.5], 
+    [2.0, 5.0, -1.0, 2.0], 
+    [-1.5, 2.7, 3.3, -0.8]]
 
-weights = [[0.2, 0.8, -0.5, 1.0 ], 
-           [0.5, -0.91, 0.26, -0.5], 
-           [-0.26, -0.27, 0.17, 0.87]]
+class Layer_Dense:
+    def __init__ (self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, -0.5 ], 
-           [-0.5, 0.12, -0.33], 
-           [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_ouputs = np.dot(inputs, np.array(weights).T) + biases #watch shape errors
-layer2_ouputs = np.dot(layer1_ouputs, np.array(weights2).T) + biases2 #watch shape errors
-
-print(layer2_ouputs)
+Layer_1 = Layer_Dense(4, 5 )
+Layer_2 = Layer_Dense(5, 5 ) #input needs to be output from previous layer
 
 
-
-
-
-
- 
-
+Layer_1.forward(X)
+print(Layer_1.output)
+Layer_2.forward(Layer_1.output)
+print(Layer_2.output)
